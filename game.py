47e886bb4f6d3c5f806f1269149573064b56e79b
BenGorr <ggbengaben@gmail.com>
2020-12-28 15:16:54 +0800

    Basic concept and added Player class

---
+++ game.py
@@ -0,0 +1,67 @@
+# simple snakes game with pygame
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+
+#import and initiate pygame
+from player import Player
+import pygame
+import time
+pygame.init()
+
+from config import *
+#Set up drawing window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Snake game by Ben')
+
+#Initiate a player object, in this case a snake
+snake = Player()
+
+#Run game until user ask to quit
+running = True
+while running:
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        # did the user hit a KEY
+        if event.type == KEYDOWN:
+            # if ESCAPE key is pressed
+            if event.key == K_ESCAPE:
+                running = False
+        # if user clicked close button
+        if event.type == QUIT:
+            running = False
+
+    #Variables
+    snake_speed = 30
+    clock = pygame.time.Clock()
+
+    #Get all the keys pressed
+    pressed_keys = pygame.key.get_pressed()
+
+    #update snake every frame
+    collided = snake.update(pressed_keys)
+    #if snake collided
+    if (collided):
+        running = False
+    # Fill the background with white
+    screen.fill((238, 238, 238))
+
+    # draw snake onto screen
+    screen.blit(snake.surf, snake.rect)
+
+    #flip the display screen (use this to update the content)
+    #pygame.display.flip()
+    pygame.display.update()
+    clock.tick(snake_speed)
+
+# Quit game here
+pygame.quit()
---
+++ player.py
@@ -0,0 +1,43 @@
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+
+import pygame
+from config import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface(SNAKE_SIZE)
+        self.surf.fill(SNAKE_COLOR)
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        #move sprite based on key pressed
+        if (pressed_keys[K_UP]):
+            self.rect.move_ip(0, -SNAKE_BLOCK)
+        if (pressed_keys[K_DOWN]):
+            self.rect.move_ip(0, SNAKE_BLOCK)
+        if (pressed_keys[K_LEFT]):
+            self.rect.move_ip(-SNAKE_BLOCK, 0)
+        if (pressed_keys[K_RIGHT]):
+            self.rect.move_ip(SNAKE_BLOCK, 0)
+
+        #prevent sprite going off screen
+        if (self.rect.top < 0):
+            return True
+        if (self.rect.bottom > SCREEN_HEIGHT):
+            return True
+        if (self.rect.left < 0):
+            return True
+        if (self.rect.right > SCREEN_WIDTH):
+            return True
+        return False

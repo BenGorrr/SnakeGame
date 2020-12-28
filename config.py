4bb1d6aa8b10ddc2aa3c58cbd6897ac1e06c79b7
BenGorr <ggbengaben@gmail.com>
2020-12-28 15:15:29 +0800

    Added config file to store some constants

---
+++ config.py
@@ -0,0 +1,7 @@
+# Define constants for the screen width and height
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+SNAKE_SIZE = (20, 20) #XY
+SNAKE_COLOR = (0, 200, 0) #RGB
+SNAKE_BLOCK = 10 #pixels between each move / frame update
